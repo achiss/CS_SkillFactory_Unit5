cd5acba525ed306b77269cb681ab764f4093ae27
Aleksandr S <87600035+achiss@users.noreply.github.com>
2023-08-17 00:39:55 +0300

    202308.17 - temp

--- Program.cs
+++ Program.cs
@@ -1,24 +1,25 @@
-﻿
-//
-//
 
-namespace Unit5_result
-{
-    class Program
-    {
-        private static void Main(string[] args)                     // main method
-        {
-            UserData();
-            ShowUserData();
+// resulted task, Unit 5
+// august 2023
+
+namespace Unit5_result {
+
+    class Program {
+
+        static (string Name, string Surname, int Age, string Pet, string[] PetList, string[] FavFlowers) UserList;
+
+        private static void Main(string[] args) {
+            UserData(ref UserList);
+            ShowUserData(UserList);
 
             Console.ReadKey();
         }
 
-        public static string GetDataFromConsole()                   // Получение строчки 
-        {
-            string data = Console.ReadLine();
-            
-            if(string.IsNullOrEmpty(data))                          // ! IsNullOrEmpty() - есть ли аналоги?
+
+        public static string GetDataFromConsole() {
+            string dataString = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(dataString))                          // ! IsNullOrEmpty() - есть ли аналоги?
             {
                 Console.WriteLine("Данные не были введены.");
                 Console.Write("Введите данные: ");
@@ -26,18 +27,18 @@ namespace Unit5_result
             }
             else
             {
-                return data;
+                return dataString;
             }
         }
 
-        public static int GetNumberDataFromConsole()                // Получение числовых значений 
-        {
-            int number;
-            bool CheckNumber = int.TryParse(GetDataFromConsole(), out number);
 
-            if(CheckNumber)
+        public static int GetNumberDataFromConsole() {
+            int numberString;
+            bool CheckNumberString = int.TryParse(GetDataFromConsole(), out numberString);
+
+            if (CheckNumberString)
             {
-                return number;
+                return numberString;
             }
             else
             {
@@ -47,28 +48,57 @@ namespace Unit5_result
             }
         }
 
-        public static void UserData()
-        {
-            (string Name, string Surname, int Age, string Pet, string[] PetCount, string[] FavFlowers) UserList;
 
+        public static void UserData(ref (string Name, string Surname, int Age, string Pet, string[] PetList, string[] FavFlowers) tuple) {
             Console.Write("Введите имя: ");
-            UserList.Name = GetDataFromConsole();
-            
+            tuple.Name = GetDataFromConsole();
+
             Console.Write("Введите фамилию: ");
-            UserList.Surname = GetDataFromConsole();
+            tuple.Surname = GetDataFromConsole();
 
             Console.Write("Введите возраст: ");
-            UserList.Age = GetNumberDataFromConsole();
-        }
+            tuple.Age = GetNumberDataFromConsole();
 
-        public static void GetArraysUserData()
-        {
+            Console.WriteLine("Есть ли у вас животные? (да или нет)");
+            tuple.Pet = GetDataFromConsole().ToLower();
+            if ((tuple.Pet == "нет") || (tuple.Pet == "н"))
+            {
+                tuple.Pet = "Домашних питомцев нет.";
+                tuple.PetList = new string[0];
+            }
+            else
+            {
+                Console.WriteLine("Сколько питомцев Вы имеете? (ответ цифрами)");
+                tuple.PetList[0] = Console.ReadLine();
+                if (tuple.PetList.Length <= 2)
+                {
+                    Console.Write("Введите кличку Вашего питомца: ");
+                    tuple.PetList[1] = GetDataFromConsole();
+                }
+                else
+                {
+                    for (int i = 1; i < int.Parse(tuple.PetList[0]); i++)
+                    {
+                        Console.Write($"Введите кличку {i} питомца");
+                        tuple.PetList[i] = GetDataFromConsole();
+                    }
+                }
+            }
 
-        }
+            Console.WriteLine("Сколько цветов Вам нравится? (ответ цифрами)");
+            int FavFlowersCount = GetNumberDataFromConsole();
+            for (int i = 0; i < FavFlowersCount; i++)
+            {
+                Console.Write("Введите цвет:");
+                tuple.FavFlowers[i] = GetDataFromConsole();
+            }
+        }   
 
-        public static void ShowUserData()
-        {
-            Console.WriteLine($"Добрый день {}");
+
+        public static void ShowUserData((string Name, string Surname, int Age, string Pet, string[] PetList, string[] FavFlowers) tuple) {
+            Console.WriteLine($"\n\tДобрый день {tuple.Surname} {tuple.Name}, Ваш возраст {tuple.Age}" +
+                $"\n\t" +
+                $"\n\tВаши любимые цветы: ");
         }
     }
 }
