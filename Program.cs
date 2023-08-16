a1c0ceb11d7c2b67fc9ddf8975c3775cc14a6d60
S-Achi-S <irosru@gmail.com>
2023-08-16 16:55:41 +0300

    template_1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//
+//
+
+namespace Unit5_result
+{
+    class Program
+    {
+        private static void Main(string[] args)                     // main method
+        {
+
+        }
+
+        public static string GetDataFromConsole()
+        {
+            string data = Console.ReadLine();
+            
+            if(data == null)
+            {
+                Console.WriteLine("Данные не были введены.");
+                return GetDataFromConsole();
+            }
+            else
+            {
+                return data;
+            }
+        }
+
+        public static int GetNumberDataFromConsole()
+        {
+            int number = int.Parse(GetDataFromConsole());
+            
+            if((number == 0) || (number < 0))
+            {
+                Console.WriteLine("Введите корректные значения (не могут быть меньше или равны 0).");
+                return GetNumberDataFromConsole();
+            }
+            else
+            {
+                return number;
+            }
+        }
+
+        public static bool CheckValueDataFromConsole()              // need to update!
+        {
+            bool check = false;
+            return check;
+        }
+
+
+    }
+}
